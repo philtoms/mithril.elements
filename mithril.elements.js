3a4e8cdca03346b44100c8ec84760bdf131bf736
philtoms <phil.toms@hotmail.co.uk>
2015-03-13 13:22:07 +0000

    composable version

--- mithril.elements.js
+++ mithril.elements.js
@@ -9,12 +9,29 @@
 'use strict';
 var m = (function app(window, mithril) {
 
-  var STRING = '[object String]', ARRAY = "[object Array]";
+  var OBJECT = '[object Object]', STRING = '[object String]', ARRAY = "[object Array]";
   var type = {}.toString;
+  var graph={}, unloads=[], elements={};
 
-  // save the mithril API
+  // save the mithril API 
   mithril = mithril || require('mithril');
   var render = mithril.render;
+
+  function merge(obj1,obj2,filter) {
+    var classAttrName = 'class' in obj1 ? 'class' : 'className';
+    var classes = obj1[classAttrName]|| '';
+    Object.keys(obj2).forEach(function(k){
+      if (k.indexOf('class')>=0){
+        classes += ' ' + obj2[k];
+      } else if ((filter||' ').indexOf(k)<0) {
+        obj1[k] = obj2[k];
+      }
+    }); 
+    if (classes && classes.trim()) {
+      obj1[classAttrName]=classes.trim();
+    }
+    return obj1;
+  }
 
   /*
    * compose a digraph of the current v-dom
@@ -31,35 +48,38 @@ var m = (function app(window, mithril) {
       });
     }
     if (!cell.tag) return cell;
-    var data = cell, attrs = cell.attrs, children = cell.children;
+    var module = elements[cell.tag], attrs = cell.attrs, children = cell.children, data = {tag:cell.tag,attrs:attrs,children:[]};
 
     // calculate position for new node and check if it already exists.
     // Note that model entity id takes precidence over node position - 
     //  handles model state change at a specific node position, eg:
     //    data list changes
-    var id = (attrs.id || attrs.key)? 'id' + (attrs.id || '') + (attrs.key || '') : d + cell.tag + p + (Object.keys(attrs).join()||'');
+    var id = cell.tag + ((attrs.state && attrs.state.id)? attrs.state.id : d + '' + p);
     var node = graph[id];
 
     // composition: [module -> ctrl] -> children -> [view] -> cell
-    if (cell.module) {
-      node = node || new (cell.module.controller || function(){})(ctx);
+    if (module && !node) {
+      node = new (module.controller || function(s,c){this.state=s; this.ctx=c;})(attrs.state || {},ctx);
+      if (node.onunload) unloads.push(node.onunload);
+      graph[id] =  node; 
     }
     for (var i=0, l=children.length; i<l; i++) {
       data.children[i] = compose(children[i], id, i, node);
     }
-    if (cell.module) {
+    if (module) {
       node.inner = children.length>1? children:children[0];
       node.attrs = cell.attrs;
-      if (node.onunload) unloads.push(node.onunload);
-      data = compose(cell.module.view(node),d,p,ctx);
-      if (data.attrs) m.merge(data.attrs,attrs)
+      data = compose(module.view(node),d,p,ctx);
+      if (data.attrs) merge(data.attrs,attrs,'state');
     }
 
-    if (node) graph[id] =  node; 
+    // tidy up tag
+    if (data.tag && data.tag[0]==='$'){
+      data.tag=data.tag.substr(1);
+    }
     return data;
   }
 
-  var graph={}, unloads=[];
   mithril.render = function(root, cell, forceRecreation) {
     // key into mithril page lifecycle
     if (m.redraw.strategy() === 'all'){
@@ -73,34 +93,15 @@ var m = (function app(window, mithril) {
     render(root, cell, forceRecreation);
   }
 
-  var m = function(module) { 
-    var tag = module.view? '$elm' : module;
-    var args = [tag].concat([].slice.call(arguments,1));
-    var cell = mithril.apply(null,args);
-    if (module.view) {
-      cell.module = module;
-    }
-    return cell;
+  mithril.element = function(tag, module){
+    if (type.call(tag) !== STRING) throw new Error('tag m.element(tag, module) should be a string');
+    if (elements[tag]) throw new Error('tag "' + tag + '" already registered as element');
+    // nothing more to do here, element initialization is lazily
+    // deferred to first redraw
+    return (elements[tag] = module);
   };
 
-  m.merge = function(obj1,obj2,filter) {
-    var classAttrName = 'class' in obj1 ? 'class' : 'className';
-    var classes = obj1[classAttrName]|| '';
-    Object.keys(obj2).forEach(function(k){
-      if (k.indexOf('class')>=0){
-        classes += ' ' + obj2[k];
-      } else if ((filter||' ').indexOf(k)<0) {
-        obj1[k] = obj2[k];
-      }
-    }); 
-    if (classes && classes.trim()) {
-      obj1[classAttrName]=classes.trim();
-    }
-    return obj1;
-  }
-
-  // build the new API
-  return m.merge(m,mithril);
+  return mithril;
 
 })(typeof window != "undefined" ? window : {},m);
 
